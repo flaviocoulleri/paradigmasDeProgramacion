b97279d2577de825b5042657bf02b56205676fe6
Flavio Coulleri <104596145+flaviocoulleri@users.noreply.github.com>
2025-04-08 18:53:41 -0300

    Update ejercicio4.c

--- ejercicio4.c
+++ ejercicio4.c
@@ -1,21 +1,34 @@
 #include<stdio.h>
-typedef struct {
+typedef struct
+{
     int number,count;
-}Moda;
+} Moda;
 void ordenarVectorCount(Moda v[], int n);
 void ordenarVectorValue(Moda v[], int n);
 int main()
 {
     Moda moda[4];
-    int matrix[3][4]={{1,2,3,4},{1,2,3,4},{1,2,3,4}},
-    arrayModa[3]={0},
-    countModa=0,
-    busqActiva=0;
-
-    for(int i=0;i<3;i++)
+    int matrix[3][4]=
     {
-        for(int j=0;j<4;j++)
+        {1, 2, 2, 1},
+        {4, 4, 3, 3},
+        {5, 5, 5, 6}
+    },
+    arrayModa[3]= {0},countModa,
+                  busqActiva=0;
+
+    for(int i=0; i<3; i++)
+    {
+        for(int k=0; k<4; k++)
         {
+            moda[k].count=0;
+            moda[k].number=0;
+
+        }
+        countModa=0;
+        for(int j=0; j<4; j++)
+        {
+            busqActiva=0;
             if(countModa==0)
             {
                 moda[countModa].number=matrix[i][j];
@@ -23,34 +36,43 @@ int main()
                 countModa++;
             }
             else
-                for(int h=0;h<countModa&&busqActiva!=1;h++)
+                for(int h=0; h<countModa&&busqActiva!=1; h++)
                 {
                     if(matrix[i][j]==moda[h].number)
                     {
                         moda[h].count++;
                         busqActiva=1;
                     }
-                    if(moda[h].number==0)
-                    {
-                        moda[h].number=matrix[i][j];
-                        countModa++;
-                    }
-
                 }
+            if(busqActiva == 0)
+            {
+                moda[countModa].number = matrix[i][j];
+                moda[countModa].count = 1;
+                countModa++;
+            }
+
         }
-        ordenarVectorCount(&moda,countModa);
-        ordenarVectorValue(&moda,countModa);
-        arrayModa[i]=moda[1].number;
+        ordenarVectorValue(moda,countModa);
+        ordenarVectorCount(moda,countModa);
+        arrayModa[i]=moda[0].number;
     }
 
-
+    for(int i=0; i<3; i++)
+        printf("%d ",arrayModa[i]);
     return 0;
-}void ordenarVectorValue(Moda v[], int n) {
+}
+
+
+void ordenarVectorValue(Moda v[], int n)
+{
     int i, j;
     Moda aux;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (v[j].number > v[j + 1].number) {
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (v[j].number < v[j + 1].number)
+            {
                 aux = v[j];
                 v[j] = v[j + 1];
                 v[j + 1] = aux;
@@ -59,13 +81,16 @@ int main()
     }
 }
 
-// Ordena por el campo count
-void ordenarVectorCount(Moda v[], int n) {
+void ordenarVectorCount(Moda v[], int n)
+{
     int i, j;
     Moda aux;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (v[j].count > v[j + 1].count) {
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (v[j].count < v[j + 1].count)
+            {
                 aux = v[j];
                 v[j] = v[j + 1];
                 v[j + 1] = aux;
@@ -73,3 +98,4 @@ void ordenarVectorCount(Moda v[], int n) {
         }
     }
 }
+
